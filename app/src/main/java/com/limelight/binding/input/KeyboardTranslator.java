28940eaa49861948bc73ff9d48faa6bc60af9377
Zen3515 <7106408+Zen3515@users.noreply.github.com>
2026-03-17 20:19:46 +0700

    Add support for KEYCODE_NUMPAD_ENTER

--- app/src/main/java/com/limelight/binding/input/KeyboardTranslator.java
+++ app/src/main/java/com/limelight/binding/input/KeyboardTranslator.java
@@ -239,6 +239,7 @@ public class KeyboardTranslator implements InputManager.InputDeviceListener {
                 break;
                 
             case KeyEvent.KEYCODE_ENTER:
+            case KeyEvent.KEYCODE_NUMPAD_ENTER:
                 translated = 0x0d;
                 break;
 
